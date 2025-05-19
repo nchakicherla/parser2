38efd2fba47a15cebc1acf7a0699d3721dcbcce6
nchakicherla <nchaki6@icloud.com>
2025-05-19 02:51:48 -0700

    lay out error structure; naming convention in error.h/.c

--- src/rkp_error.h
+++ src/rkp_error.h
@@ -2,13 +2,14 @@
 #define RKP_ERROR_H
 
 #include <stddef.h>
+#include <inttypes.h>
 
-typedef enum {
-	RKP_SEV_DEBUG,
-	RKP_SEV_INFO,
-	RKP_SEV_ERROR,
-	RKP_SEV_FATAL
-} RKPSeverity;
+typedef enum { // ultimately determined by flags but general conventions provided
+	RKP_SEV_DEBUG, // stderr, log
+	RKP_SEV_INFO, // stdout, log
+	RKP_SEV_ERROR, // stderr, stdout, log
+	RKP_SEV_FATAL // stderr, stdout, log
+} e_rkp_error_severity;
 
 typedef enum {
 	RKP_ERR_TYPE_NONE,
@@ -16,7 +17,7 @@ typedef enum {
 	RKP_ERR_TYPE_IO,
 	RKP_ERR_TYPE_MEM,
 	RKP_ERR_TYPE_MATH
-} RKPErrorType;
+} e_rkp_error_type;
 
 typedef enum {
 	RKP_ERR_CTX_EXTERNAL,
@@ -25,18 +26,28 @@ typedef enum {
 	RKP_ERR_CTX_TRANSPILER,
 	RKP_ERR_CTX_VM,
 	RKP_ERR_CTX_RUNTIME
-} RKPErrorContext;
+} e_rkp_error_context;
 
-typedef struct {
-	RKPSeverity severity;
-	RKPErrorType type;
-	RKPErrorContext context;
+typedef enum {
+	RKP_ERR_FLAG_NONE		= 0,
+	RKP_ERR_FLAG_USER_MSG	= 1 << 0,
+	RKP_ERR_FLAG_LOG_ONLY	= 1 << 1,
+	RKP_ERR_FLAG_DEV_ONLY	= 1 << 2,
+	RKP_ERR_FLAG_CRITICAL	= 1 << 3,
+	RKP_ERR_FLAG_HALT		= 1 << 4
+} e_rkp_error_flags;
+
+typedef struct s_rkp_error {
+	e_rkp_error_severity severity;
+	e_rkp_error_type type;
+	e_rkp_error_context context;
 	const char *msg;
 	const char *file;
 	size_t line;
 	size_t column;
 	int code;
-} Error;
+	uint32_t flags;
+} rkp_error;
 
 int rkp_error_set_log_file(char *filename);
 int rkp_error_close_log_file(void);
